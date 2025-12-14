9ffc2a69640e1b2ba6081d5a0d6d7b434bc1366d
MrRedShark77 <mrredshark77@gmail.com>
2025-12-14 12:50:24 +0300

    test 3

---
+++ src/data/textures.ts
@@ -0,0 +1,4 @@
+export const texturesURL = import.meta.glob('@assets/textures/*.{png,jpg,jpeg,PNG,JPEG}', { eager: true, query: '?url', import: 'default' }) as Record<string, string>
+
+export const generatorTextures = ['', ...new Array(10).fill('').map((_,i) => `/src/assets/textures/generator-${i+1}.png`).map(x => texturesURL[x])]
+export const infinityGeneratorTextures = ['', ...new Array(10).fill('').map((_,i) => `/src/assets/textures/inf-generator-${i+1}.png`).map(x => texturesURL[x])]
